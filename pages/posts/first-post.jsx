165a85cd18b339be2f0a410b0316ff1b1006526d
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-04-09 08:47:15 -0300

    - Added a ProfileComponent component - Added a Head to /posts/first-post

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,8 +1,22 @@
 import Link from "next/link"
+import Head from "next/head"
+import Image from "next/image"
+
+const ProfileComponent = () => {
+  <Image
+    src="/images/profile.jpg"
+    height={144}
+    width={144}
+    alt="Henrique Kirch Heck"
+  />
+}
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">

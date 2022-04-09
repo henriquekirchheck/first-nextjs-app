2f20826937b27ab1d9f0649544434d03947adf5e
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-04-09 08:52:55 -0300

    Formatting changes

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,13 +1,13 @@
-import Link from "next/link"
-import Head from "next/head"
-import Image from "next/image"
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
 
 const ProfileComponent = () => {
   <Image
-    src="/images/profile.jpg"
+    src='/images/profile.jpg'
     height={144}
     width={144}
-    alt="Henrique Kirch Heck"
+    alt='Henrique Kirch Heck'
   />
 }
 
@@ -19,7 +19,7 @@ export default function FirstPost() {
       </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">
+        <Link href='/'>
           <a>Back to home</a>
         </Link>
       </h2>

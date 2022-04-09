7e6d30235508a0d3763b1fcb1b542c1c751eb634
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-04-09 17:25:17 -0300

    Did some changes to UX of posts/[id]

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,5 +1,7 @@
 import { Layout } from '../../components/Layout'
+import { Date } from '../../components/Date'
 import { getAllPostsIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 import Head from 'next/head'
 
 export async function getStaticPaths() {
@@ -25,13 +27,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml}} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
